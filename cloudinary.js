63960b2058a78f77cf39a2f1512fe50119b8eac7
rohandagale-dev <rohandagale-dev@gmail.com>
2025-02-08 12:47:02 +0530

    project init v1.0.0

--- cloudinary.js
+++ cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2
 
 cloudinary.config({
-  cloud_name: 'dgh263nmt',
-  api_key: '641842339472793',
-  api_secret: '6PxsxCmGoEaquDgAyiU37Gg9FlE',
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 })
 
 module.exports = cloudinary
